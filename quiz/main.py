b95bf575f487d10a7c5e8e3f646ca64ab82ecb58
sayanote <sanotchi.hamu702@gmail.com>
2026-02-08 22:39:04 +0900

    ADD: health API

--- quiz/main.py
+++ quiz/main.py
@@ -1,6 +1,19 @@
-def main():
-    print("Hello from quiz!")
+# ------------
+# main.py
+# ------------
+# This is the entrypoint of Quiz Service APIs
+
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get("/health")
+def health():
+    return "OK"
 
 
 if __name__ == "__main__":
-    main()
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8989)

38775e088dc905ad233fdb54b8477fd5d626e8d0
sayanote <sanotchi.hamu702@gmail.com>
2026-02-25 23:06:57 +0900

    feat(quizzes): add correct_answer and wrong_options to ChoiceQuiz model

--- manager/service/quizzes/models.py
+++ manager/service/quizzes/models.py
@@ -3,9 +3,14 @@ from django.core.exceptions import ValidationError
 
 class ChoiceQuiz(models.Model):
     text = models.CharField(max_length=128)
+    correct_answer = models.CharField(max_length=128)
+    wrong_option_1 = models.CharField(max_length=128)
+    wrong_option_2 = models.CharField(max_length=128, blank=True, default="")
+    wrong_option_3 = models.CharField(max_length=128, blank=True, default="")
     created_at = models.DateTimeField(auto_now_add=True)
     frequency = models.PositiveIntegerField(default=0)
     n_of_correct = models.PositiveIntegerField(default=0)
+    
 
     def clean(self):
         super().clean()
@@ -16,3 +21,9 @@ class ChoiceQuiz(models.Model):
 
     def __str__(self):
         return self.text
+
+    def correct_rate(self) -> float:
+        if self.frequency > 0:
+            return 0
+        else:
+            return self.n_of_correct / self.frequency

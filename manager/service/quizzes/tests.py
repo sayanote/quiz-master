40fa5bb37eaf26f4ee598a4faece7220495db136
sayanote <sanotchi.hamu702@gmail.com>
2026-02-27 18:42:52 +0900

    Modify: get choicequiz api

--- manager/service/quizzes/tests.py
+++ manager/service/quizzes/tests.py
@@ -1,3 +1,43 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from .models import ChoiceQuiz
 
-# Create your tests here.
+class QuizAPITests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.quiz = ChoiceQuiz.objects.create(
+            text="Who is the gym leader of Asagi City?",
+            correct_answer="Mikan",
+            wrong_option_1="Hayato",
+            wrong_option_2="Tsukushi",
+            wrong_option_3="Akane",
+            frequency=10,
+            n_of_correct=5
+        )
+
+    def test_get_quiz_detail(self):
+        """
+        Test that the quiz detail API returns the correct data for a given quiz ID.
+        """
+        url = reverse('quiz_detail', args=[self.quiz.id])
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+        self.assertEqual(data['id'], self.quiz.id)
+        self.assertEqual(data['text'], self.quiz.text)
+        self.assertEqual(data['correct_answer'], self.quiz.correct_answer)
+        self.assertEqual(data['frequency'], 10)
+        self.assertEqual(data['n_of_correct'], 5)
+
+    def test_get_quiz_not_found(self):
+        """
+        Test that the API returns 404 for a non-existent quiz ID.
+        """
+        import sys
+
+        # use max number of Integer
+        int_max = sys.maxsize
+        url = reverse('quiz_detail', args=[int_max])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
--- manager/service/quizzes/views.py
+++ manager/service/quizzes/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from .models import ChoiceQuiz
 
-# Create your views here.
+def quiz_detail(request, quiz_id):
+    quiz = get_object_or_404(ChoiceQuiz, pk=quiz_id)
+    data = {
+        "id": quiz.id,
+        "text": quiz.text,
+        "correct_answer": quiz.correct_answer,
+        "wrong_option_1": quiz.wrong_option_1,
+        "wrong_option_2": quiz.wrong_option_2,
+        "wrong_option_3": quiz.wrong_option_3,
+        "created_at": quiz.created_at.isoformat(),
+        "frequency": quiz.frequency,
+        "n_of_correct": quiz.n_of_correct,
+    }
+    return JsonResponse(data)
